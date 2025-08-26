4059517f63af9fad8d006421763a9dd96a6f1421
Maryna Zemlianska <zemaruv@gmail.com>
2025-08-26 18:01:13 +0300

    Minify files.Optimization

--- index.php
+++ index.php
@@ -21,7 +21,7 @@ if (empty($_SESSION['csrf_token'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/normalize.css">
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/style.min.css">
     <title>Iphone 14 PRO</title>
 </head>
 
@@ -218,7 +218,7 @@ if (empty($_SESSION['csrf_token'])) {
             <p>Заявку успішно надіслано!</p>
         </div>
     </div>
-     <script src="/js/main.js"></script>
+    <script src="js/main.min.js"></script>
 </body>
 </html>
 
